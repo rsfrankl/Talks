996fd164818b797a48e27915cf42f161e8eda323
Rachel Franklin <rachel.franklin@newcastle.ac.uk>
2024-10-29 18:26:32 +0000

    new talks

---
+++ SCAG/SCAG_county_pop_change_CAestimates.R
@@ -0,0 +1,41 @@
+#code for looking at 2022-2023 population change in California
+library(readxl)         # for reading in Excel data
+library(dplyr)          # for data manipulation
+library(tidyr)          # for data shaping
+library(ggplot2)        # for generating the visualizations
+library(hrbrthemes) # for themes
+
+
+#uses county population by age data in SGAC folder
+ca_county_data <- read_excel("~/Documents/GitHub/SCAG/California_age_2022_2023.xlsx", sheet = "CA_estimates")
+
+head(ca_county_data)
+
+#population <- ca_county_data %>%
+
+new_data <- ca_county_data %>%
+  mutate(SCAG = as.factor(SCAG))
+ggplot(ca_county_data, aes(County, pop_change_22_23, colour = SCAG)) +
+  geom_point()+
+  coord_flip()
+
+# Plot population change 2022-2023
+new_data <- ca_county_data %>%
+  mutate(SCAG = as.factor(SCAG))
+ggplot(new_data) +
+  geom_segment( aes(x=County, xend=County, y=0, yend=pop_change_22_23), color="grey") +
+  geom_point( aes(x=County, y=pop_change_22_23, color=SCAG, size=total_2024)) +
+  coord_flip() +
+  scale_x_discrete(limits=rev) +
+  theme_dark() +
+  theme(
+    legend.position = "none",
+  ) +
+  theme(axis.line = element_line(color='darkgrey'),
+        text = element_text(family = "serif"),
+        plot.background = element_rect(fill = "#fcefeb", color = NA),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank())+
+  xlab("") +
+  ylab("California County Population Change (%), Jan 2023-Jan 2024 (CA Dept of Finance)")
